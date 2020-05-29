dc2eb2b0100956da403e5cd3cce6e2791b061b13
Nikita khanna <nikita.khanna@tothenew.com>
2020-05-29 11:17:39 +0530

    initial commit

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "93fd8dfa8f5dca8de0bd";
+/******/ 	var hotCurrentHash = "40b6e2e6af5d1eda3393";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
